edeed8e26426b60f0de42baebe631dffbf3aa41b
Irina <iargina.irina@gmail.com>
2022-12-03 17:58:54 +0200

    pagination-style

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,6 +1,7 @@
 import { createFilmItemMarkup } from './main-initial-films';
 import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
+/* import 'tui-pagination/dist/tui-pagination.css'; */
+
 import { smoothScroll } from './scroll-to-top';
 import { Notify } from 'notiflix';
 
@@ -12,6 +13,23 @@ export function pagination(total_results, filmsArray, api, keyword) {
     totalItems: total_results,
     itemsPerPage: filmsArray.length,
     visiblePages: 5,
+    centerAlign: true,
+    template: {
+      page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+      currentPage: '<a class="tui-page-btn tui-is-selected">{{page}}</a>',
+      moveButton:
+          '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+              '<span class="tui-ico-{{type}}"></span>' +
+          '</a>',
+      disabledMoveButton:
+          '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+              '<span class="tui-ico-{{type}}"></span>' +
+          '</span>',
+      moreButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+              '<span class="tui-ico-ellip">...</span>' +
+          '</a>'
+  }
   });
   pagination.on('afterMove', async event => {
     const currentPage = event.page;
@@ -30,3 +48,4 @@ export function pagination(total_results, filmsArray, api, keyword) {
     }
   });
 }
+
